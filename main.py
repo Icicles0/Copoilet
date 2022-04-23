987db5b68ad0ef80325d7e2ced40f301ef61a6a9
Icicles <76971046+IloveCats01@users.noreply.github.com>
2022-04-23 14:54:34 -0400

    Added Spikes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 import sys
+import random
+
 
 def game():
     print("You are a helecopter pilot. One day you are assigned a mission to attack a russian base")
@@ -14,12 +16,20 @@ def game():
     pygame.display.set_icon(pygame.image.load("python.png"))
     # show base.jpg
     base = pygame.image.load("base.jpg")
+    # load spikes.png
+    spikes = pygame.image.load("spikes.png")
+    # create 10 variables for x and y coordinates of the spikes
+    x_spikes = [random.randint(0, 800) for i in range(10)]
+    y_spikes = [random.randint(0, 600) for i in range(10)]
     # create window loop
     playerx = 100
     playery = 100
     while True:
-        # fill the screen with a color red
-        screen.fill((255, 0, 0))
+        # fill the screen with a color white
+        screen.fill((255, 255, 255))
+        # show spikes.png in random places
+        for i in range(0, 5):
+            screen.blit(spikes, (x_spikes[i], y_spikes[i]))
         # draw base.jpg on the screen
         screen.blit(base, (600, 400))
         # if touching base print "You win"
@@ -28,6 +38,13 @@ def game():
             pygame.quit()
             input("You win\n")
             sys.exit()
+        # if touching spikes print "You lose"
+        for i in range(0, 5):
+            if playerx > x_spikes[i] and playerx < x_spikes[i] + 100 and playery > y_spikes[i] and playery < y_spikes[i] + 100:
+                os.system("cls")
+                pygame.quit()
+                input("You lose\n")
+                sys.exit()
         # draw a rectangle as for the player
         pygame.draw.rect(screen, (0, 255, 0), (playerx, playery, 50, 50))
         # update the window
@@ -36,6 +53,7 @@ def game():
         for event in pygame.event.get():
             
             # implement a movment system
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     playery -= 5
@@ -46,12 +64,13 @@ def game():
                 if event.key == pygame.K_d:
                     playerx += 5
                 if event.key == pygame.K_k:
-                    playery -= 5
+                    playery -= 20
                 if event.key == pygame.K_j:
-                    playery += 5
+                    playery += 20
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     quit()
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
